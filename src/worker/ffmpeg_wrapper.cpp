5e7e4feecac4f9c2d81b6503742199aa8a339728
Fyodor <bocharovfedor@gmail.com>
2015-11-04 17:43:29 +0300

    Refactored ffmpeg_wrapper.

--- src/worker/ffmpeg_wrapper.cpp
+++ src/worker/ffmpeg_wrapper.cpp
@@ -10,29 +10,21 @@ const QString ffmpeg_wrapper::ffmpeg_params_           = " -loglevel quiet -i %1
 
 //ffmpeg -i input.avi -vcodec h264 -threads 0 -r 25 -g 50 -b 500k -bt 500k -acodec mp3 -ar 44100 -ab 32k out.mp4
 
-ffmpeg_wrapper::ffmpeg_wrapper()
-{
-}
-
-ffmpeg_wrapper::~ffmpeg_wrapper()
-{
-}
-
 QByteArray& ffmpeg_wrapper::encode(QByteArray &data)
 {
-     QFile temp_file(ffmpeg_tempfile_name_);
-     if (temp_file.open(QIODevice::WriteOnly))
-     {
-         temp_file.write(data);
-         temp_file.close();
-         QString cmd = ffmpeg_cmd_ + ffmpeg_params_.arg(ffmpeg_tempfile_name_, ffmpeg_output_filename_);
-         system(cmd.toStdString().c_str());
-         QFile output_file(ffmpeg_output_filename_);
-         if (output_file.open(QIODevice::ReadOnly))
-         {
-             data = output_file.readAll();
-         }
-     }
+	QFile temp_file(ffmpeg_tempfile_name_);
+	if (temp_file.open(QIODevice::WriteOnly))
+	{
+		temp_file.write(data);
+		temp_file.close();
+		QString cmd = ffmpeg_cmd_ + ffmpeg_params_.arg(ffmpeg_tempfile_name_, ffmpeg_output_filename_);
+		system(cmd.toStdString().c_str());
+		QFile output_file(ffmpeg_output_filename_);
+		if (output_file.open(QIODevice::ReadOnly))
+		{
+			data = output_file.readAll();
+		}
+	}
 
-     return data;
+	return data;
 }
