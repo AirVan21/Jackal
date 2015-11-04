fdae2d0b7396b88d91a6e5bfacee07dd6cd3b497
edgarzhavoronkov <edzhavoronkov@gmail.com>
2015-11-04 17:23:51 +0300

    Client logic stub

--- src/client/client_logic.cpp
+++ src/client/client_logic.cpp
@@ -3,3 +3,13 @@
 client_logic::client_logic()
 {
 }
+
+void client_logic::recieve_workers(QVector<QPair<QHostAddress, quint16> > ip_ports)
+{
+
+}
+
+void client_logic::recieve_chunk(const QHostAddress &ip, quint16 port, quint32 chunk_id, QByteArray chunk)
+{
+
+}
--- src/client/client_logic.h
+++ src/client/client_logic.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <QVector>
+#include <QHostAddress>
+#include <QPair>
+
 class client_logic
 {
 public:
     client_logic();
+    void recieve_workers(QVector<QPair<QHostAddress, quint16> > ip_ports);
+    void recieve_chunk(QHostAddress const & ip, quint16 port, quint32 chunk_id, QByteArray chunk);
 };
 
