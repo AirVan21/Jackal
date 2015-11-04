e027443d699ec8d71dba09b770fd2914f0b19c7a
baton2809 <butapro7@gmail.com>
2015-11-04 23:18:34 +0300

    Add GUI improvement

--- src/client/mainwindow.cpp
+++ src/client/mainwindow.cpp
@@ -34,36 +34,71 @@ MainWindow::~MainWindow()
 
 void MainWindow::init_comboboxes()
 {
-    ui->frameRateComboBox->addItem("25", QVariant(25));
-    ui->frameRateComboBox->addItem("30", QVariant(30));
-    ui->frameRateComboBox->addItem("50", QVariant(50));
+    ui->frameRateComboBox->addItem("25p", QVariant(25));
+    ui->frameRateComboBox->addItem("30p", QVariant(30));
+    ui->frameRateComboBox->addItem("48p", QVariant(48));
+    ui->frameRateComboBox->addItem("50i", QVariant(50));
+    ui->frameRateComboBox->addItem("60i", QVariant(60));
+    ui->frameRateComboBox->addItem("72p", QVariant(60));
+    ui->frameRateComboBox->addItem("90p", QVariant(90));
+    ui->frameRateComboBox->addItem("100p", QVariant(100));
+    ui->frameRateComboBox->addItem("120p", QVariant(120));
+    ui->frameRateComboBox->addItem("144 fps", QVariant(144));
+    ui->frameRateComboBox->addItem("300 fps", QVariant(300));
 
-    ui->sizeComboBox->addItem("1080x720", QVariant(QSize(1080, 720)));
-    ui->sizeComboBox->addItem("1020x1000", QVariant(QSize(1020, 1000)));
-    ui->sizeComboBox->addItem("720x480", QVariant(QSize(720, 480)));
+
+    ui->sizeComboBox->addItem("320x240", QVariant(QSize(320, 240)));
+    ui->sizeComboBox->addItem("480x576", QVariant(QSize(480, 576)));
+    ui->sizeComboBox->addItem("640x480", QVariant(QSize(640, 480)));
+    ui->sizeComboBox->addItem("720x576 ", QVariant(QSize(720, 576)));
+    ui->sizeComboBox->addItem("1024x768", QVariant(QSize(1024, 768)));
+    ui->sizeComboBox->addItem("1280x800", QVariant(QSize(1280, 800)));
+    ui->sizeComboBox->addItem("1366x768", QVariant(QSize(1366, 768)));
+    ui->sizeComboBox->addItem("1280x1024", QVariant(QSize(1280, 1024)));
+    ui->sizeComboBox->addItem("1440x900", QVariant(QSize(1440, 900)));
+    ui->sizeComboBox->addItem("1920x1080", QVariant(QSize(1920, 1080)));
+    ui->sizeComboBox->addItem("1152x864", QVariant(QSize(1152, 864)));
+    ui->sizeComboBox->addItem("1360x768", QVariant(QSize(1360, 768)));
     
-    ui->audioRateComboBox->addItem("44100", QVariant(44100));
-    ui->audioRateComboBox->addItem("47250", QVariant(47250));
-    ui->audioRateComboBox->addItem("80000", QVariant(80000));
+    ui->audioRateComboBox->addItem("44100 Hz", QVariant(44100));
+    ui->audioRateComboBox->addItem("48000 Hz", QVariant(48000));
+    ui->audioRateComboBox->addItem("96000 Hz", QVariant(96000));
+    ui->audioRateComboBox->addItem("192000 Hz", QVariant(192000));
+    ui->audioRateComboBox->addItem("2822400 Hz", QVariant(2822400));
+    ui->audioRateComboBox->addItem("5644800 Hz", QVariant(5644800));
     
+    ui->codecComboBox->addItem("mpeg4", QVariant(QString("mpeg4")));
+    ui->codecComboBox->addItem("aac", QVariant(QString("aac")));
     ui->codecComboBox->addItem("h264", QVariant(QString("h264")));
-    ui->codecComboBox->addItem("x264", QVariant(QString("x264")));
+    ui->codecComboBox->addItem("vp6", QVariant(QString("vp6")));
     
-    ui->videoBitrateComboBox->addItem("800k", QVariant(800));
-    ui->videoBitrateComboBox->addItem("1150k", QVariant(1150));
-    ui->videoBitrateComboBox->addItem("2500k", QVariant(2500));
+    ui->videoBitrateComboBox->addItem("576 kb/s", QVariant(576));
+    ui->videoBitrateComboBox->addItem("896 kb/s", QVariant(896));
+    ui->videoBitrateComboBox->addItem("1088 kb/s", QVariant(1088));
+    ui->videoBitrateComboBox->addItem("1216 kb/s", QVariant(1216));
+    ui->videoBitrateComboBox->addItem("1536 kb/s", QVariant(1536));
+    ui->videoBitrateComboBox->addItem("1856 kb/s", QVariant(1856));
+    ui->videoBitrateComboBox->addItem("2176 kb/s", QVariant(2176));
+    ui->videoBitrateComboBox->addItem("2496 kb/s", QVariant(2496));
+    ui->videoBitrateComboBox->addItem("3072 kb/s", QVariant(3072));
+    ui->videoBitrateComboBox->addItem("4992 kb/s", QVariant(4992));
+    ui->videoBitrateComboBox->addItem("7552 kb/s", QVariant(7552));
+    ui->videoBitrateComboBox->addItem("20000 kb/s", QVariant(20000));
 
-    ui->audioBitrateComboBox->addItem("256k", QVariant(256));
-    ui->audioBitrateComboBox->addItem("400k", QVariant(400));
-    ui->audioBitrateComboBox->addItem("1440k", QVariant(1440));
+    ui->audioBitrateComboBox->addItem("32 kb/s", QVariant(32));
+    ui->audioBitrateComboBox->addItem("64 kb/s", QVariant(64));
+    ui->audioBitrateComboBox->addItem("128 kb/s", QVariant(128));
+    ui->audioBitrateComboBox->addItem("192 kb/s", QVariant(192));
+    ui->audioBitrateComboBox->addItem("256 kb/s", QVariant(256));
+    ui->audioBitrateComboBox->addItem("320 kb/s", QVariant(320));
+
 }
 
-// проверка инициализации
 bool MainWindow::checkFileName()
 {
     if (parameters.file_name.fileName().isEmpty())
     {
-        cout << "file is not choosen" << endl;
+        ui->statusBar->showMessage("Choose File.", 3000);
         return false;
     }
     else
@@ -71,7 +106,7 @@ bool MainWindow::checkFileName()
         QFileInfo checkFile(ui->pathLine->text());
         if (!(checkFile.exists() && checkFile.isFile()))
         {
-            cout << "no file is found" << endl;
+            ui->statusBar->showMessage("Error: Incorrect File!", 3000);
             return false;
         }
         else
@@ -83,9 +118,10 @@ bool MainWindow::checkFileName()
                   ext == "mov"   ||
                   ext == "mpeg"  ||
                   ext == "mpeg4" ||
-                  ext == "wmv"))
+                  ext == "wmv"   ||
+                  ext == "flv"))
             {
-                cout << "not valid file extension" << endl;
+                ui->statusBar->showMessage("Error: Invalid File Extension!", 3000);
                 return false;
             }
         }
@@ -97,7 +133,7 @@ void MainWindow::open()
 {
     parameters.file_name.setFileName(QFileDialog::getOpenFileName(this, tr("Open File"), "/",
                                                                   tr("Video File (*.mp4 *.avi *.mkv *.mov "
-                                                                     "*.mpeg *.mpeg4 *.wmv)")));
+                                                                     "*.mpeg *.mpeg4 *.wmv *.flv )")));
 
     QFileInfo file_info(parameters.file_name.fileName());
     QString path(file_info.filePath());
@@ -151,10 +187,10 @@ void MainWindow::start()
     cout << "          size: " << parameters.size.width() << "x" << parameters.size.height() << endl;
     cout << "         codec: " << parameters.codec << endl;
     cout << "    frame rate: " << parameters.frame_rate << endl;
-    cout << "    audio rate: " << parameters.audio_rate << endl;
-    cout << " video bitrate: " << parameters.video_bitrate << endl;
-    cout << " audio bitrate: " << parameters.audio_bitrate << "k" << endl;
-    cout << " frame density: " << parameters.frame_density << "k" << endl;
+    cout << "    audio rate: " << parameters.audio_rate << " Hz"<< endl;
+    cout << " video bitrate: " << parameters.video_bitrate << " kb/s" << endl;
+    cout << " audio bitrate: " << parameters.audio_bitrate << " kb/s" << endl;
+    cout << " frame density: " << parameters.frame_density << endl;
 
     // Call method to send video file and parameters
 
