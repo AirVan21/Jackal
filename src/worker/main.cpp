05321d0d2b5c243e6d9b37eb3b1c08d99a4751cb
Fyodor <bocharovfedor@gmail.com>
2015-11-05 02:08:00 +0300

    Fixed worker main.

--- src/worker/main.cpp
+++ src/worker/main.cpp
@@ -8,12 +8,8 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
-//	worker w(9090);
-//	w.connect_to_server(QHostAddress("127.0.0.1"), 8080);
-
-	socket s(QHostAddress("127.0.0.1"), 8080, nullptr);
-	auto msg = create_message<number_message>(message_type::worker_server_connect, 0);
-	s.send(*msg);
+	worker w(9090);
+	w.connect_to_server(QHostAddress("127.0.0.1"), 8080);
 
 	return a.exec();
 }
